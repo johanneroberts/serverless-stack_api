6ca1ffae16f045b2670eff23f32864a28a110925
Johannes Roberts <johannes.roberts@iress.com>
2020-08-03 22:09:16 +0200

    fixed

--- create.js
+++ create.js
@@ -9,6 +9,11 @@ export const main = handler(async (event, context) => {
       userId: event.requestContext.identity.cognitoIdentityId,
       job_id: data.job_id,
       job_location: data.job_location,
+      job_suburb: data.job_suburb,
+      job_town: data.job_town,
+      job_cfc: data.job_cfc,
+      job_ffc: data.jobf_ffc,
+      job_fault_date: data.job_fault_date,
       job_plumber: data.job_plumber,
       job_status: data.job_status
     }
--- update.js
+++ update.js
@@ -9,9 +9,14 @@ export const main = handler(async (event, context) => {
       userId: event.requestContext.identity.cognitoIdentityId,
       job_id: event.pathParameters.job_id
     },
-    UpdateExpression: "SET job_location = :job_location, job_plumber = :job_plumber, job_status = :job_status",
+    UpdateExpression: "SET job_location = :job_location, job_suburb = :job_suburb, job_town = :job_town, job_cfc = :job_cfc, job_ffc = :job_ffc,  job_plumber = :job_plumber, job_fault_date = :job_fault_date, job_status = :job_status",
     ExpressionAttributeValues: {
       ":job_location": data.job_location || null,
+      ":job_job_suburb": data.job_suburb || null,
+      ":job_town": data.job_town || null,
+      ":job_cfc": data.job_cfc || null,
+      ":job_ffc": data.job_ffc || null,
+      ":job_fault_date" : data.job_fault_date || null,
       ":job_plumber": data.job_plumber || null,
       ":job_status": data.job_status || null
     },
